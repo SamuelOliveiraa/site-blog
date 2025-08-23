8519333490b6e00a4f5d4a89932b72e3551f77f7
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2025-08-23 18:28:18 -0300

    Adicionando metadata para o blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,27 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio.",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://site-blog-rho.vercel.app/og-image.png",
+    siteName: "Site.set",
+    locale: "pt-BR",
+    images: [
+      {
+        url: "https://site-blog-rho.vercel.app/og-image.png",
+        width: 800,
+        height: 600,
+        alt: "Site.set"
+      }
+    ]
+  }
+};
 
 export default function BlogListPage() {
   const sortedPost = allPosts.sort(
